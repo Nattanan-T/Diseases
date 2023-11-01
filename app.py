0369558d8dbc3138039d3a7bebba01386a666227
Nattanan-T <122088382+Nattanan-T@users.noreply.github.com>
2023-11-01 09:08:40 +0700

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from Predict import Predict_disease
 
 img,classes = Predict_disease(r'C:/Users/nattanan.t/Desktop/Personal_Files/FMS_ID/rice-blast-disease.jpg')
-    
+
+img.save('result.jpg')
+print(classes)  
     
